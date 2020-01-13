6638596d64369e416737096a9730052d892ddc47
Lazer032 <joverveldd@gmail.com>
2020-01-13 10:01:36 +0100

    Array en randomwaarden toegevoegd

--- Step1/Yahtzee/MainWindow.xaml.cs
+++ Step1/Yahtzee/MainWindow.xaml.cs
@@ -24,5 +24,21 @@ namespace Yahtzee
         {
             InitializeComponent();
         }
+
+        private void rollen_Click(object sender, RoutedEventArgs e)
+        {
+            int dobbel1 = rnd.Next(1, 6);
+            int dobbel2 = rnd.Next(1, 6);
+            int dobbel3 = rnd.Next(1, 6);
+            int dobbel4 = rnd.Next(1, 6);
+            int dobbel5 = rnd.Next(1, 6);
+            int[] Scores = { dobbel1, dobbel2, dobbel3, dobbel4, dobbel5 };
+            this.dobbeltt1.Text = Convert.ToString(dobbel1);
+            this.dobbeltt2.Text = Convert.ToString(dobbel2);
+            this.dobbeltt3.Text = Convert.ToString(dobbel3);
+            this.dobbeltt4.Text = Convert.ToString(dobbel4);
+            this.dobbeltt5.Text = Convert.ToString(dobbel5);
+        }
+        public Random rnd = new Random();
     }
 }
