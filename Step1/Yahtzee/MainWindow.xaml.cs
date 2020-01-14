e900750f5f8cb02b573e8aa6cf1980b06b3beebd
Sam Elfring <0318893@student.rocvantwente.nl>
2020-01-14 10:56:05 +0100

    Score systeem toegevoegd

--- Step1/Yahtzee/MainWindow.xaml.cs
+++ Step1/Yahtzee/MainWindow.xaml.cs
@@ -40,6 +40,8 @@ namespace Yahtzee
         public bool alGestoptDobbelsteen3 = false;
         public bool alGestoptDobbelsteen4 = false;
         public bool alGestoptDobbelsteen5 = false;
+        //Score van de speler
+        int score;
 
         private void rollen_Click(object sender, RoutedEventArgs e)
         {
@@ -69,6 +71,7 @@ namespace Yahtzee
                     dobbeltt5.Text = Convert.ToString(dobbelsteen5);
                 }
                 aantalGooien +=1;
+                AllesGestopt();
             }
             else
             {
@@ -96,6 +99,8 @@ namespace Yahtzee
                 dobbeltt1.Text = vast;
                 dobbeltt1.IsEnabled = false;
                 alGestoptDobbelsteen1 = true;
+                dobbelVast1Knop.Opacity = 0;
+                AllesGestopt();
             }
         }
         private void DobbelVast2Klik(object sender, RoutedEventArgs e)
@@ -106,6 +111,8 @@ namespace Yahtzee
                 dobbeltt2.Text = vast;
                 dobbeltt2.IsEnabled = false;
                 alGestoptDobbelsteen2 = true;
+                dobbelVast2Knop.Opacity = 0;
+                AllesGestopt();
             }
         }
         private void DobbelVast3Klik(object sender, RoutedEventArgs e)
@@ -116,6 +123,8 @@ namespace Yahtzee
                 dobbeltt3.Text = vast;
                 dobbeltt3.IsEnabled = false;
                 alGestoptDobbelsteen3 = true;
+                dobbelVast3Knop.Opacity = 0;
+                AllesGestopt();
             }
         }
         private void DobbelVast4Klik(object sender, RoutedEventArgs e)
@@ -126,6 +135,8 @@ namespace Yahtzee
                 dobbeltt4.Text = vast;
                 dobbeltt4.IsEnabled = false;
                 alGestoptDobbelsteen4 = true;
+                dobbelVast4Knop.Opacity = 0;
+                AllesGestopt();
             }
         }
         private void DobbelVast5Klik(object sender, RoutedEventArgs e)
@@ -136,13 +147,15 @@ namespace Yahtzee
                 dobbeltt5.Text = vast;
                 dobbeltt5.IsEnabled = false;
                 alGestoptDobbelsteen5 = true;
+                dobbelVast5Knop.Opacity = 0;
+                AllesGestopt();
             }
         }
         private void AllesGestopt()
         {
             if (alGestoptDobbelsteen1 == true && alGestoptDobbelsteen2 == true && alGestoptDobbelsteen3 == true && alGestoptDobbelsteen4 == true && alGestoptDobbelsteen5 == true)
             {
-
+                Punten();
             }
             else if (aantalGooien > 3)
             {
@@ -152,6 +165,52 @@ namespace Yahtzee
                 dobbeltt3.IsEnabled = false;
                 dobbeltt4.IsEnabled = false;
                 dobbeltt5.IsEnabled = false;
+
+                Punten();
+            }
+        }
+        private void Punten()
+        {
+            dobbelsteen1 = Convert.ToInt32(dobbeltt1.Text);
+            dobbelsteen2 = Convert.ToInt32(dobbeltt2.Text);
+            dobbelsteen3 = Convert.ToInt32(dobbeltt3.Text);
+            dobbelsteen4 = Convert.ToInt32(dobbeltt4.Text);
+            dobbelsteen5 = Convert.ToInt32(dobbeltt5.Text);
+            int[] dobbelstenen = {dobbelsteen1, dobbelsteen2, dobbelsteen3, dobbelsteen4, dobbelsteen5};
+            int totaalScore = dobbelsteen1 + dobbelsteen2 + dobbelsteen3 + dobbelsteen4 + dobbelsteen5;
+
+            /*Drie gelijke: De score is het totaal van alle ogen, als er minstens 3 dobbelstenen met hetzelfde aantal ogen zijn.
+              Vier gelijke: De score is het totaal van alle ogen, als er minstens 4 dobbelstenen met hetzelfde aantal ogen zijn.
+              Kleine straat: 30 punten voor 4 opeenvolgende ogenaantallen.
+              Grote straat: 40 punten voor 5 opeenvolgende ogenaantallen.
+              Full House: 25 punten voor 3 gelijke en één paar.
+              Kans: De score is het totaal aantal ogen van alle dobbelstenen.
+              Yahtzee: 50 punten als alle dobbelstenen hetzelfde aantal ogen hebben.*/
+
+            //Grote Straat
+            if (Array.Exists(dobbelstenen, element => element == 1) && Array.Exists(dobbelstenen, element => element == 2) && Array.Exists(dobbelstenen, element => element == 3) && Array.Exists(dobbelstenen, element => element == 4) && Array.Exists(dobbelstenen, element => element == 5))
+            {
+                score += 40;
+                scoreTekst.Text = Convert.ToString(score);
+            }
+
+            //Yahtzee
+            if (dobbelstenen[0] == dobbelstenen[1])
+            {
+                if(dobbelstenen[1] == dobbelstenen[2])
+                {
+                    if(dobbelstenen[2] == dobbelstenen[3])
+                    {
+                        if (dobbelstenen[3] == dobbelstenen[4])
+                        {
+                            if (dobbelstenen[4] == dobbelstenen[5])
+                            {
+                                score += 50;
+                                scoreTekst.Text = Convert.ToString(score);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
